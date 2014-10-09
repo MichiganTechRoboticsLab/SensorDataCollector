e5fb9d66947dbfd6ae0745cbd445a793b2fb5556
Dereck Wonnacott <dereck@gmail.com>
2014-10-09 14:05:26 -0400

    Camera live feed added/fixed

--- display_scripts/ImageViewer.py
+++ display_scripts/ImageViewer.py
@@ -1,24 +1,26 @@
-#!/usr/bin/env python
+#!/usr/bin/python
+
 import glob
 import cv2
 import numpy
 import os
 
-#images = glob.glob("/home/ubuntu/IRLLibs/data_storage/pics/*.bmp")
-#images.sort()
-#for image in images:
-#    img = cv2.imread(image)
-#    cv2.imshow('frame',img)
-#    if cv2.waitKey(1) & 0xFF == ord('q'):
-#        break
-
-#cv2.destroyAllWindows()
-os.chdir("/home/ubuntu/IRLLibs/data_storage/pics")
+os.chdir("data_storage/pics")
 while True:
-    image = sorted(glob.glob("*.bmp"))[-2]
-    img = cv2.imread(image)
-    cv2.imshow('UAV',img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-         break
+
+	# Find the latest file
+    filename = sorted(glob.glob("*.bmp"))[-2]
+	
+	# Open it
+    img     = cv2.imread(filename)
+	
+	# Resize it for viewing
+    img_sm  = cv2.resize(img, (0,0), fx=0.3, fy=0.3) 
+	
+	# Show it
+    cv2.imshow('RGB',img_sm)
+	
+	# If 'q' key is pressed, exit
+    if cv2.waitKey(1) & 0xFF == ord('q'): break
 
 cv2.destroyAllWindows()
